cc810da4cf36910331a98727660e63bdf87624e4
Thiago Costa <thiago.costa@vortal.biz>
2026-01-26 23:25:49 +0000

    Updating Assert on pedidos.spec

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -10,13 +10,10 @@ test('deve consultar um pedido aprovado', async ({ page }) => {
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
   // Act
-  await page.getByTestId('search-order-id').fill('VLO-19H18S')
-  await page.getByTestId('search-order-button').click()
+  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill('VLO-19H18S')
+  await page.getByRole('button', { name: 'Buscar Pedido' }).click()
 
   // Assert
-  await expect(page.getByTestId('order-result-id')).toBeVisible()
-  await expect(page.getByTestId('order-result-id')).toContainText('VLO-19H18S')
-
-  await expect(page.getByTestId('order-result-status')).toBeVisible()
-  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
+  await expect(page.getByText('VLO-19H18S')).toBeVisible()
+  await expect(page.getByText('APROVADO')).toBeVisible()
 })
