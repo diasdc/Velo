d6b2c742b6d046941c08edd24b126e7de68eb780
Thiago Costa <thiago.costa@vortal.biz>
2026-02-09 23:38:07 +0000

    Adding helpers file

---
+++ playwright/support/helpers.ts
@@ -0,0 +1,12 @@
+export function generateOrderCode() {
+    const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const digits = '0123456789';
+  
+    const randomLetters = (length) =>
+      Array.from({ length }, () => letters[Math.floor(Math.random() * letters.length)]).join('');
+  
+    const randomDigits = (length) =>
+      Array.from({ length }, () => digits[Math.floor(Math.random() * digits.length)]).join('');
+  
+    return `${randomLetters(3)}-${randomDigits(2)}${randomLetters(1)}${randomDigits(2)}${randomLetters(1)}`;
+  }
